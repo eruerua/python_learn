5db6993f0a6f15af3d2bd907f7de5350dcfa1863
eruerua <eruerame@gmail.com>
2020-03-05 22:21:41 +0800

    结束游戏

--- project_alien_game/settings.py
+++ project_alien_game/settings.py
@@ -4,17 +4,18 @@ class Settings():
         self.screen_width=1200
         self.screen_height=800
         self.bg_color=(230,230,230)
-        #设置飞船速度
+        #设置飞船
         self.ship_speed_factor = 1.5
+        self.ship_limit = 3
         #子弹设置
-        self.bullet_speed_factor=1
+        self.bullet_speed_factor = 3
         self.bullet_width=3
         self.bullet_height=15
         self.bullet_color=60,60,60
         self.bullets_allowed=3
         #外星人设置
         self.alien_speed_factor=1
-        self.fleet_drop_speed=1
+        self.fleet_drop_speed=50
         #fleet_direction为1表示向右移，为-1表示向左移
         self.fleet_direction=1
 
